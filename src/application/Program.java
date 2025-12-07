4af58e6e6edd1e5bb1282c1819dba23a55b88669
Paulo Abílio <paulofernan73@gmail.com>
2025-12-07 08:28:09 +0100

    primeira classe criada, classe position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package application;
+
+import boardgame.Position;
 
 /**
  *
@@ -14,7 +12,9 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
     
 }
