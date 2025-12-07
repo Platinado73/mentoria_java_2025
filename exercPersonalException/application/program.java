94ae80c0c908cca36b241f87f896edefb915ad38
Paulo Abílio <paulofernan73@gmail.com>
2025-12-07 02:44:22 +0100

    começando a primeira versão do exercíco sobre personalizar exceptions...

---
+++ exercPersonalException/application/program.java
@@ -0,0 +1,16 @@
+package application;
+
+/**
+ *
+ * @author PLATINADO
+ */
+public class program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
