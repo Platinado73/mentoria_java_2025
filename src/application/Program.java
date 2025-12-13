caea970ec87e1b993c3f048578883f83a6a4fffa
Paulo Abílio <paulofernan73@gmail.com>
2025-12-13 15:49:50 +0100

    Handing exceptions and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,9 +2,11 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -17,26 +19,37 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
 
         ChessMatch chessMatch = new ChessMatch();
-        
-        while(true){
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("sourcde: ");
-            ChessPosition source = UI.readChessPosition(sc);
-            
-            System.out.println();
-            
-            System.out.print("target: ");
-            ChessPosition target = UI.readChessPosition(sc);
-            
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-            
+
+        while (true) {
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("sourcde: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+                System.out.println();
+
+                System.out.print("target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+            }
+            catch(ChessException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch(InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+
         }
-        
 
     }
 
